bfb4f939f69ed9ad6262845e41db02a4b700a1a7
Nonso Andrew <nonsoandrew1@gmail.com>
2023-08-01 04:38:37 +0100

    Feat: Social Auth/Oauth

---
+++ app/Controllers/Http/SocialAuthController.ts
@@ -0,0 +1,47 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+
+export default class SocialAuthController {
+  public async redirect({ ally, params }: HttpContextContract) {
+    return ally.use(params.provider).redirect()
+  }
+
+  public async callback({ ally, auth, response, params }: HttpContextContract) {
+    const provider = ally.use(params.provider)
+    console.log(provider)
+    /**
+     * User has explicitly denied the login request
+     */
+    if (provider.accessDenied()) {
+      return 'Access was denied'
+    }
+
+    /**
+     * Unable to verify the CSRF state
+     */
+    if (provider.stateMisMatch()) {
+      return 'Request expired. Retry again'
+    }
+
+    /**
+     * There was an unknown error during the redirect
+     */
+    if (provider.hasError()) {
+      return provider.getError()
+    }
+
+    /**
+     * Finally, access the user
+     */
+    const oauthUser = await provider.user()
+    const user = await User.firstOrCreate(
+      { email: oauthUser.email },
+      {
+        fullname: oauthUser.name,
+        email: oauthUser.email,
+        oauthProvider: params.provider,
+        oauthProviderId: oauthUser.id,
+      }
+    )
+  }
+}
---
+++ config/ally.ts
@@ -0,0 +1,59 @@
+/**
+ * Config source: https://git.io/JOdi5
+ *
+ * Feel free to let us know via PR, if you find something broken in this config
+ * file.
+ */
+
+import Env from '@ioc:Adonis/Core/Env'
+import { AllyConfig } from '@ioc:Adonis/Addons/Ally'
+
+/*
+|--------------------------------------------------------------------------
+| Ally Config
+|--------------------------------------------------------------------------
+|
+| The `AllyConfig` relies on the `SocialProviders` interface which is
+| defined inside `contracts/ally.ts` file.
+|
+*/
+const allyConfig: AllyConfig = {
+  /*
+	|--------------------------------------------------------------------------
+	| Github driver
+	|--------------------------------------------------------------------------
+	*/
+  github: {
+    driver: 'github',
+    clientId: Env.get('GITHUB_CLIENT_ID'),
+    clientSecret: Env.get('GITHUB_CLIENT_SECRET'),
+    callbackUrl: 'http://localhost:3333/api/github/callback',
+  },
+  /*
+	|--------------------------------------------------------------------------
+	| Google driver
+	|--------------------------------------------------------------------------
+	*/
+  google: {
+    driver: 'google',
+    clientId: Env.get('GOOGLE_CLIENT_ID'),
+    clientSecret: Env.get('GOOGLE_CLIENT_SECRET'),
+    callbackUrl: 'https://localhost:3333/api/google/callback',
+    scopes: ['userinfo.profile'],
+    prompt: 'select_account',
+    accessType: 'offline',
+  },
+  /*
+  |--------------------------------------------------------------------------
+  | Facebook driver
+  |--------------------------------------------------------------------------
+  */
+  facebook: {
+    driver: 'facebook',
+    clientId: Env.get('FACEBOOK_CLIENT_ID'),
+    clientSecret: Env.get('FACEBOOK_CLIENT_SECRET'),
+    callbackUrl: 'http://localhost:3333/api/facebook/callback',
+  },
+}
+
+export default allyConfig
